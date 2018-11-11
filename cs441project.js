81a4d78c39f9a2bd9ea7be6d4a1e1bf26d094317
ShaneLaskowski <lasko002@cougars.csusm.edu>
2018-11-11 15:00:45 -0800

    fixed the google map not displaying error. initialized to canada

--- cs441project.js
+++ cs441project.js
@@ -1,6 +1,6 @@
 var map, infoWindow;
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {center: {lat: -34.397, lng: 150.644}, zoom: 6});
+	map = new google.maps.Map(document.getElementById('map'), {center: {lat: 56.1304, lng: -106.3468}, zoom: 6});
 	infoWindow = new google.maps.InfoWindow;
 
 	if (navigator.geolocation) {
